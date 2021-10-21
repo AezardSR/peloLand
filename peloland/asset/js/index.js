04e0796cb9ed929eaea6a68a1d4d0f7f3ea705b5
AezardSR <69091066+AezardSR@users.noreply.github.com>
2021-10-21 12:07:31 +0200

    modif  menu burger
    changement de button

--- peloland/asset/js/index.js
+++ peloland/asset/js/index.js
@@ -7,12 +7,27 @@ a.onmouseout = function() {
     hdr.classList.remove("titreOpa");
 }
 
-/*---------------------------*/
+/*------------menu burger---------------*/
 let menuB = document.getElementById("menu-button");
 let buttonBurger = document.getElementById("menuBurger");
+let buttonBurgerClose = document.getElementById("closeBurger");
+let iconUp = document.getElementById("menuBurger");
+let iconClose = document.getElementById("closeBurger");
 
 buttonBurger.onclick = function() {
     menuB.classList.toggle("closeMenu");
     menuB.classList.toggle("openMenu");
-    hdr.classList.toggle("titreOpa")
+    hdr.classList.toggle("titreOpa");
+    iconUp.classList.toggle("iconBDisplayNone");
+    iconClose.classList.toggle("iconBDisplayUp");
+    iconClose.classList.toggle("iconBDisplayNone");
+}
+buttonBurgerClose.onclick = function() {
+    menuB.classList.toggle("closeMenu");
+    menuB.classList.toggle("openMenu");
+    hdr.classList.toggle("titreOpa");
+    iconUp.classList.toggle("iconBDisplayUp");
+    iconUp.classList.toggle("iconBDisplayNone");
+    iconClose.classList.toggle("iconBDisplayUp");
+    iconClose.classList.toggle("iconBDisplayNone");
 }
